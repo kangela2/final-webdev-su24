716739267483647643f81479f06fb337d5f38536
Angela Kang <angela.kang39@myhunter.cuny.edu>
2024-08-16 20:37:01 -0400

    move markup to SingleTaskView and setup linking from all tasks to single task

--- src/store/employeesSlice.js
+++ src/store/employeesSlice.js
@@ -13,12 +13,12 @@ export function employeesReducer(state = initialState, action) {
 //API calls go here
 import axios from "axios";
 //PATH (should be where your server is running)
-const PATH = "https://jsonplaceholder.typicode.com";
+const PATH = "https://localhost:5001/api"; //path to api port from postgres
 
 //Thunk 
 export const fetchEmployees = () => async (dispatch) => {
   try {
-    let res = await axios.get(`${PATH}/users`);
+    let res = await axios.get(`${PATH}/employees`); //resource is called employees from database
     dispatch({type: 'employees/employeesLoaded', payload: res.data});
   } catch(err) {
     console.error(err);
