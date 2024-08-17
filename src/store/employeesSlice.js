4ed9659cac3e2d7a7a5b2d2573bfa118f8f3294e
Angela Kang <angela.kang39@myhunter.cuny.edu>
2024-08-16 23:51:36 -0400

    api change to localhost

--- src/store/employeesSlice.js
+++ src/store/employeesSlice.js
@@ -13,12 +13,12 @@ export function employeesReducer(state = initialState, action) {
 //API calls go here
 import axios from "axios";
 //PATH (should be where your server is running)
-const PATH = "https://localhost:5001/api"; //path to api port from postgres
+const PATH = "https://http://localhost:5001/api.typicode.com";
 
 //Thunk 
 export const fetchEmployees = () => async (dispatch) => {
   try {
-    let res = await axios.get(`${PATH}/employees`); //resource is called employees from database
+    let res = await axios.get(`${PATH}/employee`);
     dispatch({type: 'employees/employeesLoaded', payload: res.data});
   } catch(err) {
     console.error(err);
