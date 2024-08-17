48672ca4b3415a6382bc5c613696fd401ea6f21b
Angela Kang <angela.kang39@myhunter.cuny.edu>
2024-08-16 20:02:54 -0400

    add delete button, border, and padding

--- src/components/views/AllTasksView.jsx
+++ src/components/views/AllTasksView.jsx
@@ -1,21 +1,55 @@
+import './styles/all-tasks.css';
+
+let ulStyle = {
+  border: "3px solid #0d0",
+  width: "70%",
+  margin: "auto",
+  overflow: "auto",
+};
+
+let liStyle = {
+  padding: "8px 16px",
+  borderBottom: "3px solid #0d0",
+  backgroundColor: "#e5f0e1",
+};
+
+let liStyleLastChild = {
+  ...liStyle,
+  borderBottom: "none",
+};
+
 import { Link } from "react-router-dom";
 
 function AllTasksView({tasks}) {
+  let deleteTask = (taskId) => {};
+
   if (!tasks.length) {
     return (
-      <div>There are no tasks.</div>  
+      <>
+        <Link to={`/`}><button>Back to Home</button></Link>
+        <div>There are no tasks.</div>
+      </>
     );
   }
   return (
-    <>
-      <ul>
-        {tasks.map((todo, idx) => (
-          <li key={todo.id}>Task #{idx+1}: {todo.title}</li>
-        ))}
-      </ul>
-      <Link to={`/`}><button>Back to Home</button></Link>
-    </>
+    <div id="bgview" style={{display: "flex", flexDirection: "column", paddingBottom: "16px"}} >
+      <Link to={`/`}><button style={{margin: "8px"}}>Back to Home</button></Link>
+      <div style={ulStyle}>
+        {tasks.map((todo, idx) => {
+          let styleBool = idx === tasks.length - 1 ? liStyleLastChild : liStyle;
+          return (
+            <div key={todo.id} style={styleBool}>
+              <h4>Task #{idx+1}: {todo.title}</h4>
+              <h5>Assigned to: Employee {todo.userId}</h5>
+              <h5>{todo.completed ? "COMPLETED" : "IN PROGRESS"}</h5>
+              <button onClick={() => deleteTask(todo.id)}>Delete</button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
+
 }
 
 export default AllTasksView;
