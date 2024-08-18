4810991e9aa216904ac801a7a23822d2892d3d2b
Angela Kang <angela.kang39@myhunter.cuny.edu>
2024-08-17 23:00:49 -0400

    updated reducer, thunk, all employees view styling and button, and container and view

--- src/components/views/AllEmployeesView.jsx
+++ src/components/views/AllEmployeesView.jsx
@@ -21,7 +21,7 @@ let liStyleLastChild = {
 
 import { Link } from "react-router-dom";
 
-function AllEmployeesView({employees}) {
+function AllEmployeesView({employees, deleteEmployee}) {
   if (!employees.length) {
     return (
       <>
@@ -32,24 +32,29 @@ function AllEmployeesView({employees}) {
     );
   }
   return (
-    <>
-       <h2>All Employees</h2>
-      <ul>
-        {employees.map((employee, idx) => (
-          <li key={employee.id}>
-            Employee #{idx+1}: 
-            <Link to={`/employees/${employee.id}`}>
-              {employee.firstname} {employee.lastname}
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <Link to={`/`}><button>Back to Home</button></Link>
-      <Link to={`/employees/new`}><button>Add Employee</button></Link>
-
-    </>
+    <div id="bgview" style={{ ...ulStyle }}>
+      <h2>All Employees</h2>
+      <Link to={`/`}><button style={{ margin: "8px" }}>Back to Home</button></Link>
+      <Link to={`/employees/new`}><button style={{ margin: "8px" }}>Add Employee</button></Link>
+      <div style={ulStyle}>
+        {employees.map((employee, idx) => {
+          let styleBool = idx === employees.length - 1 ? liStyleLastChild : liStyle;
+          return (
+            <div key={employee.id} style={styleBool}>
+              <h4>
+                Employee #{idx + 1}:{" "}
+                <Link to={`/employees/${employee.id}`}>
+                  {employee.firstname} {employee.lastname}
+                </Link>
+              </h4>
+              <h5>Department: {employee.department}</h5>
+              <button onClick={() => deleteEmployee(employee.id)}>Delete</button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
-
 }
 
 export default AllEmployeesView;
